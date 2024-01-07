a01543a6af8c0f380882caee027c7e4ae44c1b69
shuto kikai <yysskikai04@icloud.com>
2024-01-08 08:42:21 +0900

    index

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,7 +6,7 @@ from .views import IndexClass,NewClass,ShowClass
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('index',IndexClass.as_view(), name='index'),
+  path('',IndexClass.as_view(), name='index'),
   path('new',NewClass.as_view(), name='new'), 
   path('show',ShowClass.as_view(), name='show'), 
 ]
